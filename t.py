3bae51f471e894f7ac7c7a9575c3d597dfbd75b0
AlbertRipak <arspak107@gmail.com>
2021-08-07 17:28:58 +0300

    add file from two folder

---
+++ t.py
@@ -0,0 +1,9 @@
+pizza = []
+pizza.append('paperoni')
+pizza.append('cheder')
+pizza.append('musluvskia')
+pizza.insert(0, 'bekon')
+print(pizza)
+
+my_pizza = pizza[2:]
+print(my_pizza)
---
+++ tuples.py
@@ -0,0 +1,4 @@
+t = ('one', 'two', 'three')
+print(t[0])
+t = ('four', 'five', 'sex')
+print(t[2])
